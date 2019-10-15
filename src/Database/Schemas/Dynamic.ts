0b0d7df64245542a332cf914b02acd6709aeba5c
Andre Rodrigues <andre.rsilva96@hotmail.com>
2019-10-15 20:24:09 -0300

    Adding version key false

--- src/Database/Schemas/Dynamic.ts
+++ src/Database/Schemas/Dynamic.ts
@@ -1,6 +1,6 @@
 import { Document, Schema, Model, model } from 'mongoose'
 
-const DynamicSchema = new Schema({}, { strict: false })
+const DynamicSchema = new Schema({}, { strict: false, versionKey: false })
 
 export const Dynamic = (collection:string): Model<Document> => {
   return model<Document>(collection, DynamicSchema)
